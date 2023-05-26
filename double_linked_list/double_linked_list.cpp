eb868ab107219c451c32668c0c2ebf09cd881cb8
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-26 11:10:32 +0700

    memberi data pada objek baru

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -37,7 +37,8 @@ void DoubleLinkedList::addNode() {
 	cout << "\nEnter the name of the student: ";
 	cin >> nm;
 	Node* newNode = new Node(); //step 1
-	newNode->noMhs = nim;
+	newNode->noMhs = nim; // step 2
+	newNode->name = nm;
 }
 
 
