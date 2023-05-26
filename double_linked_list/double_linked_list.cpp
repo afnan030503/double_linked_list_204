e6e183b83e27cee1c151842d457ba7a245d221ba
afnan030503 <muhammadafnanmaulidi@gmail.com>
2023-05-26 11:08:25 +0700

    membuat node baru

--- double_linked_list/double_linked_list.cpp
+++ double_linked_list/double_linked_list.cpp
@@ -10,4 +10,39 @@ public:
 	Node* prev; 
 };
 
-class DoubleLinkedList{};
+class DoubleLinkedList{
+private:
+	Node* START;
+public:
+	DoubleLinkedList();
+	void addNode();
+	bool search(int ro11No, Node** current);
+	bool deleteNode(int ro11No);
+	bool listEmpty();
+	void traserve();
+	void revtraserve();
+	void hapus();
+	void searchData();
+};
+
+DoubleLinkedList::addNode() {
+	START = NULL;
+}
+
+void DoubleLinkedList::addNode() {
+	int nim;
+	string nm;
+	cout << "\nEnter the ro11 number of the student: ";
+	cin >> nim;
+	cout << "\nEnter the name of the student: ";
+	cin >> nm;
+	Node* newNode = new Node(); //step 1
+}
+
+
+
+
+
+
+
+
